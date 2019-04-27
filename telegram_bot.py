917c3991ec3bc7b825b21562c07c8b59e16d4bba
Dmitry315 <vgurskoy@mail.ru>
2019-04-27 20:48:00 +0300

    modify

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,13 @@
 # Импортируем необходимые классы.
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+reply_keyboard = [['/address', '/phone'],['/site', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 def echo(bot, update):
     # У объекта класса Updater есть поле message, являющееся
@@ -12,17 +15,34 @@ def echo(bot, update):
     # У message есть поле text, содержащее текст полученного сообщения,
     # а также метод reply_text(str), отсылающий ответ пользователю,
     # от которого получено сообщение.
-    update.message.reply_text(update.message.text)
+    update.message.reply_text('Я получил сообщение ' + update.message.text)
 
 
 def start(bot, update):
-    update.message.reply_text(
-        "Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
-
+    update.message.reply_text("Я бот-справочник. Какая информация вам нужна?", reply_markup=markup)
 
 def help(bot, update):
     update.message.reply_text(
         "Я пока не умею помогать... Я только ваше эхо.")
+
+
+def address(bot, update):
+    update.message.reply_text("Адрес: г. Москва, ул. Льва Толстого, 16")
+
+
+def phone(bot, update):
+    update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+def site(bot, update):
+    update.message.reply_text("Сайт: http://www.yandex.ru/company")
+
+
+def work_time(bot, update):
+    update.message.reply_text("Время работы: пн-пт -- 9-00 - 19-00")
+
+def close_keyboard(bot, update):
+    update.message.reply_text("Ok", reply_markup=ReplyKeyboardRemove())
 
 def main():
     # Создаём объект updater. Вместо слова "TOKEN" надо разместить
@@ -43,6 +63,11 @@ def main():
     dp.add_handler(text_handler)
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("address", address))
+    dp.add_handler(CommandHandler("phone", phone))
+    dp.add_handler(CommandHandler("site", site))
+    dp.add_handler(CommandHandler("work_time", work_time))
+    dp.add_handler(CommandHandler("close", close_keyboard))
 
     # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
